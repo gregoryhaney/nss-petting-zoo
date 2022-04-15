05bbce30657cf6bb1e590cb944e82d75e03eba43
Gregory Haney <gregory.haney@gmail.com>
2022-04-15 13:24:30 -0500

    INITIAL

---
+++ animals.py
@@ -0,0 +1,210 @@
+class Moose():
+    """this is a class for all MOOSE objects"""
+    def __init__(self, id, name, species, date_added, walking):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.walking = True
+        
+class Wildebeast():
+    """this is a class for all Wildebeast objects"""
+    def __init__(self, id, name, species, date_added, walking):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.walking = True
+        
+class Cow():
+    """this is a class for all Cow objects"""
+    def __init__(self, id, name, species, date_added, walking):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.walking = True
+        
+class Gazelle():
+    """this is a class for all Gazelle objects"""
+    def __init__(self, id, name, species, date_added, walking):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.walking = True
+
+class Reindeer():
+    """this is a class for all Reindeer objects"""
+    def __init__(self, id, name, species, date_added, walking):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.walking = True
+        
+class Worm():
+    """this is a class for all Earthworm objects"""
+    def __init__(self, id, name, species, date_added, slithering):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.slithering = True
+        
+class Slug():
+    """this is a class for all Slug objects"""
+    def __init__(self, id, name, species, date_added, slithering):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.slithering = True
+        
+class Cobra():
+    """this is a class for all Cobra objects"""
+    def __init__(self, id, name, species, date_added, slithering):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.slithering = True
+        
+class Asp():
+    """this is a class for all Asp objects"""
+    def __init__(self, id, name, species, date_added, slithering):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.slithering = True
+        
+class Leech():
+    """this is a class for all Leech objects"""
+    def __init__(self, id, name, species, date_added, slithering):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.slithering = True
+        
+class Salmon():
+    """this is a class for all Salmon objects"""
+    def __init__(self, id, name, species, date_added, swimming):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.swimming = True
+        
+class Guppy():
+    """this is a class for all Guppy objects"""
+    def __init__(self, id, name, species, date_added, swimming):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.swimming = True
+        
+class Koi():
+    """this is a class for all Koi objects"""
+    def __init__(self, id, name, species, date_added, swimming):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.swimming = True
+        
+class Oscar():
+    """this is a class for all Oscar objects"""
+    def __init__(self, id, name, species, date_added, swimming):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.swimming = True
+        
+class Bass():
+    """this is a class for all Bass objects"""
+    def __init__(self, id, name, species, date_added, swimming):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.date_added = date_added
+        self.swimming = True
+        
+
+Billy = Moose()
+Billy.name = "Marvin"
+Billy.species = "Great North Mainer"
+Billy.date_added = "20210911"
+
+Winnie = Wildebeast()
+Winnie.name = "Winnie"
+Winnie.species = "Domesticated"
+Winnie.date_added = "20190601"
+
+Cullen = Cow()
+Cullen.name = "Cullen"
+Cullen.species = "Angus Bull"
+Cullen.date_added = "20220114"
+
+Gary = Gazelle()
+Gary.name = "Gary"
+Gary.species = "Canadian"
+Gary.date_added = "20210403"
+
+Rudolph = Reindeer()
+Rudolph.name = "Rudolph"
+Rudolph.species = "Red-nosed"
+Rudolph.date_added = "20140808"
+
+Edwin = Worm()
+Edwin.name = "Edwin"
+Edwin.species = "Earthworm"
+Edwin.date_added = "20211009"
+
+Silva = Slug()
+Silva.name = "Silva"
+Silva.species = "Humpback Slug"
+Silva.date_added = "20220303"
+
+Karen = Cobra()
+Karen.name = "Karen"
+Karen.species = "Kalifornia King"
+Karen.date_added = "20190601"
+
+Alice = Asp()
+Alice.name = "Alice"
+Alice.species = "American Asian"
+Alice.date_added = "20160714"
+
+Leo = Leech()
+Leo.name = "Leo"
+Leo.species = "Anemic"
+Leo.date_added = "20200406"
+
+Steven = Salmon()
+Steven.name = "Steven"
+Steven.species = "Rainbow Speckled"
+Steven.date_added = "20190601"
+
+George = Guppy()
+George.name = "George"
+George.species = "Flag Tail"
+George.date_added = "20161108"
+
+Kevin = Koi()
+Kevin.name = "Kevin"
+Kevin.species = "Neon Orange Glow"
+Kevin.date_added = "20210309"
+
+Omar = Oscar()
+Omar.name = "Omar"
+Omar.species = "Tuxedo Pink"
+Omar.date_added = "20120919"
+
+Billy = Bass()
+Billy.name = "Billy"
+Billy.species = "Large Mouth"
+Billy.date_added = "20201215"
