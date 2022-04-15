568d0c7732b65f78e23881e4819acb605e18ddd5
Gregory Haney <gregory.haney@gmail.com>
2022-04-15 14:09:03 -0500

    MAJOR: up through Chapter 3. Classes refactored

--- animals.py
+++ animals.py
@@ -1,136 +1,275 @@
+# original class for MOOSE
+# class Moose():
+#     """this is a class for all MOOSE objects"""
+#     def __init__(self, id, name, species, date_added, walking):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.walking = True
+    
+   # refactored class for MOOSE with constructors
 class Moose():
     """this is a class for all MOOSE objects"""
-    def __init__(self, id, name, species, date_added, walking):
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.walking = True
-        
-class Wildebeast():
-    """this is a class for all Wildebeast objects"""
-    def __init__(self, id, name, species, date_added, walking):
-        self.id = id
-        self.name = name
-        self.species = species
-        self.date_added = date_added
-        self.walking = True
-        
-class Cow():
-    """this is a class for all Cow objects"""
-    def __init__(self, id, name, species, date_added, walking):
-        self.id = id
-        self.name = name
-        self.species = species
-        self.date_added = date_added
-        self.walking = True
-        
-class Gazelle():
-    """this is a class for all Gazelle objects"""
-    def __init__(self, id, name, species, date_added, walking):
-        self.id = id
-        self.name = name
-        self.species = species
-        self.date_added = date_added
         self.walking = True
 
-class Reindeer():
-    """this is a class for all Reindeer objects"""
-    def __init__(self, id, name, species, date_added, walking):
+
+    
+        
+# class Wildebeast():
+#     """this is a class for all Wildebeast objects"""
+#     def __init__(self, id, name, species, date_added, walking):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.walking = True
+
+   # refactored class for WILDEBEAST with constructors
+class Wildebeast():
+    """this is a class for all WILDEBEAST objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
+        self.walking = True
+
+# class Cow():
+#     """this is a class for all Cow objects"""
+#     def __init__(self, id, name, species, date_added, walking):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.walking = True
+        
+        
+class Cow():
+    """this is a class for all COW objects"""
+    def __init__(self, name, species):
+        self.id = id
+        self.name = name
+        self.species = species
         self.walking = True
         
-class Worm():
-    """this is a class for all Earthworm objects"""
-    def __init__(self, id, name, species, date_added, slithering):
+        
+# class Gazelle():
+#     """this is a class for all Gazelle objects"""
+#     def __init__(self, id, name, species, date_added, walking):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.walking = True
+
+
+class Gazelle():
+    """this is a class for all GAZELLE objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.slithering = True
+        self.walking = True
+        
+        
+# class Reindeer():
+#     """this is a class for all Reindeer objects"""
+#     def __init__(self, id, name, species, date_added, walking):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.walking = True
+        
+class Reindeer():
+    """this is a class for all REINDEER objects"""
+    def __init__(self, name, species):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.walking = True
+        
+# class Worm():
+#     """this is a class for all Earthworm objects"""
+#     def __init__(self, id, name, species, date_added, slithering):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.slithering = True
+        
+class Worm():
+    """this is a class for all WORM objects"""
+    def __init__(self, name, species):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.slither = True
+        
+        
+        
+# class Slug():
+#     """this is a class for all Slug objects"""
+#     def __init__(self, id, name, species, date_added, slithering):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.slithering = True
         
 class Slug():
-    """this is a class for all Slug objects"""
-    def __init__(self, id, name, species, date_added, slithering):
+    """this is a class for all SLUG objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.slithering = True
+        self.slither = True
+        
+        
+        
+# class Cobra():
+#     """this is a class for all Cobra objects"""
+#     def __init__(self, id, name, species, date_added, slithering):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.slithering = True
         
 class Cobra():
-    """this is a class for all Cobra objects"""
-    def __init__(self, id, name, species, date_added, slithering):
+    """this is a class for all COBRA objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.slithering = True
+        self.slither = True
+        
+        
+        
+# class Asp():
+#     """this is a class for all Asp objects"""
+#     def __init__(self, id, name, species, date_added, slithering):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.slithering = True
         
 class Asp():
-    """this is a class for all Asp objects"""
-    def __init__(self, id, name, species, date_added, slithering):
+    """this is a class for all ASP objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.slithering = True
+        self.slither = True
+        
+# class Leech():
+#     """this is a class for all Leech objects"""
+#     def __init__(self, id, name, species, date_added, slithering):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.slithering = True
+        
         
 class Leech():
-    """this is a class for all Leech objects"""
-    def __init__(self, id, name, species, date_added, slithering):
+    """this is a class for all LEECH objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
-        self.slithering = True
+        self.slither = True
+        
+                
+
+# class Salmon():
+#     """this is a class for all Salmon objects"""
+#     def __init__(self, id, name, species, date_added, swimming):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.swimming = True
         
 class Salmon():
-    """this is a class for all Salmon objects"""
-    def __init__(self, id, name, species, date_added, swimming):
+    """this is a class for all SALMON objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
         self.swimming = True
+        
+# class Guppy():
+#     """this is a class for all Guppy objects"""
+#     def __init__(self, id, name, species, date_added, swimming):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.swimming = True
         
 class Guppy():
-    """this is a class for all Guppy objects"""
-    def __init__(self, id, name, species, date_added, swimming):
+    """this is a class for all GUPPY objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
         self.swimming = True
+        
+# class Koi():
+#     """this is a class for all Koi objects"""
+#     def __init__(self, id, name, species, date_added, swimming):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.swimming = True
         
 class Koi():
-    """this is a class for all Koi objects"""
-    def __init__(self, id, name, species, date_added, swimming):
+    """this is a class for all KOI objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
         self.swimming = True
+        
+# class Oscar():
+#     """this is a class for all Oscar objects"""
+#     def __init__(self, id, name, species, date_added, swimming):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.swimming = True
         
 class Oscar():
-    """this is a class for all Oscar objects"""
-    def __init__(self, id, name, species, date_added, swimming):
+    """this is a class for all OSCAR objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
         self.swimming = True
         
+# class Bass():
+#     """this is a class for all Bass objects"""
+#     def __init__(self, id, name, species, date_added, swimming):
+#         self.id = id
+#         self.name = name
+#         self.species = species
+#         self.date_added = date_added
+#         self.swimming = True
+        
 class Bass():
-    """this is a class for all Bass objects"""
-    def __init__(self, id, name, species, date_added, swimming):
+    """this is a class for all BASS objects"""
+    def __init__(self, name, species):
         self.id = id
         self.name = name
         self.species = species
-        self.date_added = date_added
         self.swimming = True
         
 
@@ -208,3 +347,22 @@ Billy = Bass()
 Billy.name = "Billy"
 Billy.species = "Large Mouth"
 Billy.date_added = "20201215"
+
+
+
+# after refactoring, use these to build new objects:
+mary = Moose("Mary", "Alaskan")
+warren = Wildebeast("Warren", "Hornless")
+colton = Cow("Colton", "Holstein")
+genny = Gazelle("Genny", "Longhaired")
+rex = Reindeer("Rex", "Hairless")
+ned = Worm("Ned", "Nightcrawler")
+golda = Slug("Golda", "Garden Slug")
+colby = Cobra("Colby", "Congo")
+aaron = Asp("Aaron", "Black Mamba")
+lawrence = Leech("Lawrence", "Saltwater")
+sara = Salmon("Sara", "Murrel")
+gayle = Guppy("Gayle", "Basking")
+keith = Koi("Keith", "Barb Tail")
+oliver = Oscar("Oliver", "Beluga")
+barry = Bass("Barry", "Small Mouth")
